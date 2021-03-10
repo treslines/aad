797fc685ca3214fa212d20f87e855e341f79177b
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-03-10 22:03:37 +0100

    codelab activities and intents as fragments

--- app/src/main/java/com/softsuit/aad/codelab/userinterface/activityintents/SendFragment.kt
+++ app/src/main/java/com/softsuit/aad/codelab/userinterface/activityintents/SendFragment.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.softsuit.aad.R
+import com.softsuit.aad.codelab.util.navTo
 import com.softsuit.aad.databinding.FragmentSendBinding
 
 class SendFragment : Fragment(R.layout.fragment_send) {
     private lateinit var binding: FragmentSendBinding
+    private var reply: String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSendBinding.bind(view)
+        binding.buttonSend.setOnClickListener {
+            val args = Bundle()
+            args.putString("SEND",binding.inputSend.text.toString())
+            navTo(R.id.action_sendFragment_to_replyFragment, args)
+        }
+
+        arguments?.let {
+            reply = it.getString("REPLY", null)
+        }
+        reply?.let {
+            binding.textMessage.text = it
+        }
     }
 }
--- app/src/main/java/com/softsuit/aad/codelab/util/FragmentExt.kt
+++ app/src/main/java/com/softsuit/aad/codelab/util/FragmentExt.kt
@@ -1,7 +1,9 @@
 package com.softsuit.aad.codelab.util
 
+import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-fun Fragment.navTo(@IdRes destination: Int) = findNavController().navigate(destination)
+fun Fragment.navTo(@IdRes dest: Int) = findNavController().navigate(dest)
+fun Fragment.navTo(@IdRes dest: Int, args: Bundle) = findNavController().navigate(dest, args)
