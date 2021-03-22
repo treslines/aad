370cdf6dead647b93fb5c95346c5c9f2c17128af
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-03-22 22:08:33 +0100

    codelab room with views

--- app/src/main/java/com/softsuit/aad/MainApplication.kt
+++ app/src/main/java/com/softsuit/aad/MainApplication.kt
@@ -1,9 +1,19 @@
 package com.softsuit.aad
 
 import android.app.Application
+import com.softsuit.aad.codelab.datamanagement.roomwithview.WordRepository
+import com.softsuit.aad.codelab.datamanagement.roomwithview.WordRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import timber.log.Timber
 
 class MainApplication() : Application() {
+
+    // No need to cancel this scope as it'll be torn down with the process
+    private val applicationScope = CoroutineScope(SupervisorJob())
+    private val database by lazy { WordRoomDatabase.getDatabase(this@MainApplication, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
--- app/src/main/java/com/softsuit/aad/codelab/datamanagement/roomwithview/WordFragment.kt
+++ app/src/main/java/com/softsuit/aad/codelab/datamanagement/roomwithview/WordFragment.kt
@@ -1,8 +1,47 @@
 package com.softsuit.aad.codelab.datamanagement.roomwithview
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.softsuit.aad.MainActivity
+import com.softsuit.aad.MainApplication
 import com.softsuit.aad.R
+import com.softsuit.aad.databinding.FragmentInteractiveUiBinding
+import com.softsuit.aad.databinding.FragmentWordBinding
 
 class WordFragment : Fragment(R.layout.fragment_word) {
+
+    private val newWordActivityRequestCode = 1
+
+    private lateinit var binding: FragmentWordBinding
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((requireActivity().application as MainApplication).repository)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWordBinding.bind(view)
+        val adapter = WordListAdapter()
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.newWordFragment)
+        }
+
+        wordViewModel.allWords.observe(requireActivity(), { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.submitList(it) }
+        })
+
+        setFragmentResultListener("requestKey") { key, bundle ->
+            val result = bundle.getString("word", "empty")
+            wordViewModel.insert(Word(result))
+        }
+    }
 
 }
