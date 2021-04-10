4299302027cb1bc15bb17c7161a09409174c9663
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-04-10 15:13:33 +0200

    codelab bonus animation pulse explode

--- app/src/main/java/com/softsuit/aad/codelab/animation/PulseExplodeFragment.kt
+++ app/src/main/java/com/softsuit/aad/codelab/animation/PulseExplodeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.softsuit.aad.R
+import com.softsuit.aad.codelab.util.navTo
+import com.softsuit.aad.codelab.util.setVisible
 import com.softsuit.aad.databinding.FragmentPulseExplodeBinding
 
 class PulseExplodeFragment : Fragment(R.layout.fragment_pulse_explode) {
@@ -13,5 +15,66 @@ class PulseExplodeFragment : Fragment(R.layout.fragment_pulse_explode) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPulseExplodeBinding.bind(view)
 
+        // ==================================================
+        //  PULSE EXPLODE ANIMATION       -------------------
+        // ==================================================
+        startPulseAnim(binding.buttonPulseBackgroundAnimation)
+        binding.buttonPulse.setOnClickListener {
+            binding.buttonPulse.setVisible(false)
+            startExplodeAnim(binding.buttonPulseBackgroundAnimation, ::showNextScreen)
+        }
+    }
+
+    private fun showNextScreen() {
+        navTo(R.id.pulseExplodeDestinationFragment_with_custom_animation)
+    }
+
+    private fun startPulseAnim(view: View) = scaleDown(view)
+    private fun scaleUp(view: View) {
+        view.animate()
+            .scaleX(SCALE_UP_VALUE)
+            .scaleY(SCALE_UP_VALUE)
+            .setDuration(PULSE_DURATION)
+            .withEndAction { kotlin.run { scaleDown(view) } }
+    }
+
+    private fun scaleDown(view: View) {
+        view.animate()
+            .scaleX(SCALE_DOWN_VALUE)
+            .scaleY(SCALE_DOWN_VALUE)
+            .setDuration(PULSE_DURATION)
+            .withEndAction { kotlin.run { scaleUp(view) } }
+    }
+
+    private fun startExplodeAnim(view: View, endAction: () -> Unit) = shrink(view, endAction)
+    private fun shrink(view: View, endAction: () -> Unit) {
+        // if any previous animation is running, make sure it is canceled first
+        view.animation?.let {
+            it.cancel()
+            it.reset()
+        }
+        view.animate()
+            .scaleX(SHRINK_VALUE)
+            .scaleY(SHRINK_VALUE)
+            .setDuration(SHRINK_DURATION)
+            .withEndAction { kotlin.run { explode(view, endAction) } }
+    }
+
+    private fun explode(view: View, endAction: () -> Unit) {
+        view.animate()
+            .scaleX(EXPLODE_VALUE)
+            .scaleY(EXPLODE_VALUE)
+            .setDuration(EXPLODE_DURATION)
+            .withEndAction { kotlin.run { endAction() } }
+    }
+
+    companion object AnimProps {
+        private const val PULSE_DURATION = 1500L
+        private const val SHRINK_DURATION = 600L
+        private const val EXPLODE_DURATION = 300L
+        private const val SCALE_DOWN_VALUE = 0.9f
+        private const val SCALE_UP_VALUE = 1f
+        private const val SHRINK_VALUE = 0.3f
+        private const val EXPLODE_VALUE = 4f
     }
 }
--- app/src/main/java/com/softsuit/aad/codelab/util/ViewExt.kt
+++ app/src/main/java/com/softsuit/aad/codelab/util/ViewExt.kt
@@ -3,11 +3,19 @@ package com.softsuit.aad.codelab.util
 import android.view.View
 import android.view.animation.Animation
 
-fun View.startAnimation(anim: Animation, onEnd: () -> Unit){
-    anim.setAnimationListener(object : Animation.AnimationListener{
+fun View.startAnimation(anim: Animation, onEnd: () -> Unit) {
+    anim.setAnimationListener(object : Animation.AnimationListener {
         override fun onAnimationEnd(anim: Animation?) = onEnd()
         override fun onAnimationRepeat(anim: Animation?) = Unit
         override fun onAnimationStart(anim: Animation?) = Unit
     })
     this.startAnimation(anim)
+}
+
+fun View.setVisible(show: Boolean) {
+    if (show) {
+        this.visibility = View.VISIBLE
+    } else {
+        this.visibility = View.GONE
+    }
 }
