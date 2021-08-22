46c7c8029c1dfe3a9daa8dd2ab50d32ec3f20f9d
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-08-22 19:36:00 +0200

    codelab shared preferences

--- app/src/main/java/br/com/programadordeelite/gdc/codelab/datamanagement/sharedprefs/SharedPrefFragment.kt
+++ app/src/main/java/br/com/programadordeelite/gdc/codelab/datamanagement/sharedprefs/SharedPrefFragment.kt
@@ -4,10 +4,10 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.ColorRes
 import androidx.fragment.app.Fragment
 import br.com.programadordeelite.gdc.R
 import br.com.programadordeelite.gdc.databinding.FragmentSharedPrefBinding
-
 
 class SharedPrefFragment : Fragment(R.layout.fragment_shared_pref) {
 
@@ -18,9 +18,8 @@ class SharedPrefFragment : Fragment(R.layout.fragment_shared_pref) {
     }
 
     private var mCount = 0
-    private var mColor = 0
-    private var mPreferences: SharedPreferences? = null
-
+    @ColorRes private var mColor = R.color.black_text_color
+    private val mPreferences: SharedPreferences by lazy { requireActivity().getSharedPreferences(SHARED_PREFS, MODE_PRIVATE) }
 
     private lateinit var binding: FragmentSharedPrefBinding
 
@@ -28,44 +27,45 @@ class SharedPrefFragment : Fragment(R.layout.fragment_shared_pref) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSharedPrefBinding.bind(view)
 
-        mPreferences = requireActivity().getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
-        mPreferences?.getInt(COUNT_KEY, 0)?.let { mCount = it }
-        mPreferences?.getInt(COLOR_KEY, 0)?.let { mColor = it }
-        if (mCount != 0) {
-            binding.countText.text = String.format("%s", mCount)
-        }
-        if (mColor != 0) {
-            binding.countText.setTextColor(requireContext().getColor(mColor))
-        }
+        mPreferences.getInt(COUNT_KEY, 0).let { mCount = it }
+        mPreferences.getInt(COLOR_KEY, 0).let { mColor = it }
+
+        if (mCount != 0) binding.countText.text = String.format("%s", mCount)
+        if (mColor != 0) binding.countText.setTextColor(getColorFromResourceId(mColor))
+
         binding.reset.setOnClickListener { reset() }
         binding.blackButton.setOnClickListener {
             mColor = R.color.design_default_color_on_secondary
-            updateUi(requireContext().getColor(R.color.design_default_color_on_secondary))
+            updateUi(R.color.design_default_color_on_secondary)
         }
+
         binding.greenButton.setOnClickListener {
             mColor = R.color.design_default_color_secondary
-            updateUi(requireContext().getColor(R.color.design_default_color_secondary))
+            updateUi(R.color.design_default_color_secondary)
         }
+
         binding.blueButton.setOnClickListener {
-            mColor = R.color.design_default_color_primary
-            updateUi(requireContext().getColor(R.color.design_default_color_primary))
+            mColor = R.color.titleColor
+            updateUi(R.color.titleColor)
         }
+
         binding.redButton.setOnClickListener {
             mColor = R.color.design_default_color_error
-            updateUi(requireContext().getColor(R.color.design_default_color_error))
+            updateUi(R.color.design_default_color_error)
         }
         binding.count.setOnClickListener {
             binding.countText.text = (++mCount).toString()
         }
     }
 
-    private fun updateUi(color: Int) {
-        binding.countText.setTextColor(color)
+    private fun updateUi(@ColorRes colorId: Int) {
+        binding.countText.setTextColor(getColorFromResourceId(colorId))
     }
 
     override fun onPause() {
         super.onPause()
-        mPreferences?.let {
+        // NA HORA DO "APERREIO" DA PROVA, DA UM SWITCH PRA CÁ, LEMBRA E VOLTA! :)
+        mPreferences.let {
             val editor = it.edit()
             editor.putInt(COUNT_KEY, mCount)
             editor.putInt(COLOR_KEY, mColor)
@@ -77,13 +77,18 @@ class SharedPrefFragment : Fragment(R.layout.fragment_shared_pref) {
         mCount = 0
         binding.countText.text = String.format("%s", mCount)
 
-        mColor = 0
-        binding.countText.setBackgroundColor(mColor)
+        mColor = android.R.color.white
+        binding.countText.setBackgroundColor(getColorFromResourceId(mColor))
 
-        mPreferences?.let {
+        // NA HORA DO "APERREIO" DA PROVA, DA UM SWITCH PRA CÁ, LEMBRA E VOLTA! :)
+        mPreferences.let {
             val editor = it.edit()
             editor.clear()
             editor.apply()
         }
+    }
+
+    private fun getColorFromResourceId(@ColorRes colorId: Int) : Int {
+        return if(colorId != 0) requireContext().getColor(colorId) else R.color.black_text_color
     }
 }
