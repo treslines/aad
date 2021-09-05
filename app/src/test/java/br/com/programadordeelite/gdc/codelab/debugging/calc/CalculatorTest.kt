83da679edf02d7c32fb0da305e411682dc10f3e7
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-09-05 15:56:53 +0200

    codelab unit test

--- app/src/test/java/br/com/programadordeelite/gdc/codelab/debugging/calc/CalculatorTest.kt
+++ app/src/test/java/br/com/programadordeelite/gdc/codelab/debugging/calc/CalculatorTest.kt
@@ -36,29 +36,45 @@ class CalculatorTest {
     fun mul() = assertThat(calculator.mul(2.0, 1.5), `is`(equalTo(3.0)))
 
     @Test
-    fun add_one_positive_number_to_one_negative_number() = assertThat(calculator.add(-1.0, 2.0), `is`(equalTo(1.0)))
+    fun add_one_positive_number_to_one_negative_number() {
+        assertThat(calculator.add(-1.0, 2.0), `is`(equalTo(1.0)))
+    }
 
     @Test
-    fun `add to floats with decimal values to each other` () = assertThat(calculator.add(1.111, 1.111), `is`(equalTo(2.222)))
+    fun `add to floats with decimal values to each other` () {
+        assertThat(calculator.add(1.111, 1.111), `is`(equalTo(2.222)))
+    }
 
     @Test
-    fun subWorksWithNegativeResult() = assertThat(calculator.sub(1.0, 17.0), `is`(equalTo(-16.0)))
+    fun subWorksWithNegativeResult() {
+        assertThat(calculator.sub(1.0, 17.0), `is`(equalTo(-16.0)))
+    }
 
     @Test
-    fun divTwoNumbersThroughZero_ShouldReturn_PositiveInfinity() = assertThat(calculator.div(32.0, 0.0), `is`(equalTo(Double.POSITIVE_INFINITY)))
+    fun divTwoNumbersThroughZero_ShouldReturn_PositiveInfinity() {
+        assertThat(calculator.div(32.0, 0.0), `is`(equalTo(Double.POSITIVE_INFINITY)))
+    }
 
     @Test
-    fun divTwoNumbersZeroNegative() = assertThat(calculator.div(-32.0, 0.0), `is`(equalTo(Double.NEGATIVE_INFINITY)))
+    fun divTwoNumbersZeroNegative() {
+        assertThat(calculator.div(-32.0, 0.0), `is`(equalTo(Double.NEGATIVE_INFINITY)))
+    }
 
     @Test
-    fun divTwoNumbersNegative() = assertThat(calculator.div(32.0, -2.0), `is`(equalTo(-16.0)))
+    fun divTwoNumbersNegative() {
+        assertThat(calculator.div(32.0, -2.0), `is`(equalTo(-16.0)))
+    }
 
     // +--------------------------------------------------------------------------+
     // | Boas práticas: separar testes em blocos de positivos e negativos         |
     // +--------------------------------------------------------------------------+
     @Test
-    fun positive__divide_two_numbers_should_succeed() = assertThat(calculator.div(3.0, 2.0), `is`(equalTo(1.5)))
+    fun positive__divide_two_numbers_should_succeed() {
+        assertThat(calculator.div(3.0, 2.0), `is`(equalTo(1.5)))
+    }
 
     @Test
-    fun negative__divide_number_through_zero_should_return_infinity() = assertThat(calculator.div(10.0, 0.0), `is`(equalTo(Double.POSITIVE_INFINITY)))
+    fun negative__divide_number_through_zero_should_return_infinity() {
+        assertThat(calculator.div(10.0, 0.0), `is`(equalTo(Double.POSITIVE_INFINITY)))
+    }
 }
