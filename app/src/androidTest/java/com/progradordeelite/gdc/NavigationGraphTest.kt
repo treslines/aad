9b195cd50fae75d496447f7b32fa81382b81e80b
Ricardo Ferreira <ricardo.ferreira@soprasteria.com>
2021-09-24 21:33:37 +0200

    codelab testando navegacao

--- app/src/androidTest/java/com/progradordeelite/gdc/NavigationGraphTest.kt
+++ app/src/androidTest/java/com/progradordeelite/gdc/NavigationGraphTest.kt
@@ -1,34 +1,39 @@
 package com.progradordeelite.gdc
 
-//import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult
-//import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultBaseUtils.matchesCheckNames
-//import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultUtils.matchesViews
-//import org.hamcrest.Matchers.`is`
-//import org.hamcrest.Matchers.allOf
-import android.os.Bundle
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import br.com.programadordeelite.gdc.MainFragment
-import br.com.programadordeelite.gdc.MainFragmentDirections
+import br.com.programadordeelite.gdc.MainActivity
 import br.com.programadordeelite.gdc.R
-import br.com.programadordeelite.gdc.codelab.core.toast.ToastSnakeFragment
+import br.com.programadordeelite.gdc.codelab.debugging.calc.Calculator
+import junit.framework.TestCase.assertEquals
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.nav_host_fragment
+import kotlinx.android.synthetic.main.activity_user_navigation.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+
 
 /**
- * QUER APRENDER A TESTAR UI COM ESPRESSO? >>> VEJA O VIDEO DE ACESSIBILIDADE
+ * QUER APRENDER A TESTAR UI COM ESPRESSO?
+ * >>> VEJA O VIDEO DE ACESSIBILIDADE
  */
 @RunWith(AndroidJUnit4::class)
 @MediumTest
@@ -37,22 +42,43 @@ class NavigationGraphTest {
 
     @Test
     fun testNavigationFromMainFragmentToCodelabToastSnake() = runBlockingTest {
+        val testNavController = TestNavHostController(ApplicationProvider.getApplicationContext())
 
         // MOCKANDO O NAVIGATION CONTROLLER
-        val navController = mock(NavController::class.java)
+        mock(testNavController.javaClass)
 
-        // CRIANDO UM CENARIO DE TESTE
-        val scenario = launchFragmentInContainer<MainFragment>(Bundle(), R.style.AppTheme)
+        launchActivity<MainActivity>()
 
         // GIVEN - ADICIONANDO O NAV CONTROLLER MOCKADO AO CENARIO
-        scenario.onFragment {
-            Navigation.setViewNavController(it.view!!, navController)
-        }
+//        val fragScenario = launchFragmentInContainer<MainFragment>(Bundle(), R.style.AppTheme)
+//        fragScenario.onFragment{
+//            Navigation.setViewNavController(it.view!!, navController)
+//        }
 
         // WHEN - QUANDO USUÁRIO CLICAR NO BOTÃO PARA O CODELAB DE TOAST E SNACK
         Espresso.onView(withId(R.id.codelab_toast_snake)).perform(ViewActions.click())
 
-        // THEN - AI VERIFICA SE O NAVIGATION GRAPH REALMENTE NAVEGOU PARA LÁ
-        verify(navController).navigate(MainFragmentDirections.actionMainFragmentToToastSnakeFragment())
+        // 1°) FORMA DE VERIFICAR SE O APP NAVEGOU PARA A TELA EM QUESTÃO
+        Espresso.onView(withId(R.id.toast)).check(ViewAssertions.matches(
+            ViewMatchers.withContentDescription("tostando"))
+        )
+
+        // 2°) DE VERIFICAR NAVEGACAO
+        assertThat(testNavController.currentDestination?.id,`is`(equalTo(R.id.toastSnakeFragment)))
+
+        // 3°)DE VERIFICAR SE O NAVIGATION GRAPH REALMENTE NAVEGOU PARA LÁ
+        verify(testNavController).navigate(R.id.toastSnakeFragment)
+
+        val calculator = mock(Calculator::class.java)
+        // IMAGINE QUE AQUI SERIA UMA CHAMADA PARA O BACKEND, POREM VC NAO TEM O BACKEND AINDA
+        // AI NESSE CASO VC PODERIA RETORNAR 200 PARA SUCESSO (CODIGO HTTP DE SUCESSO)
+        Mockito.`when`(calculator.add(anyDouble(),anyDouble())).thenReturn(4.0)
+
+        // VERIFICA RESULTADO
+        val userCount = calculator.add(2.0,1.0)
+        assertEquals(4.0, userCount)
+
+        // VERIFICA COMPORTAMENTO, NESSE CASO QUE O METODO ADD FOI CHAMADO
+        verify(calculator).add(anyDouble(), anyDouble())
     }
 }
--- app/src/main/java/br/com/programadordeelite/gdc/codelab/debugging/calc/Calculator.kt
+++ app/src/main/java/br/com/programadordeelite/gdc/codelab/debugging/calc/Calculator.kt
@@ -1,13 +1,13 @@
 package br.com.programadordeelite.gdc.codelab.debugging.calc
 
-class Calculator {
+open class Calculator {
     enum class Operator { ADD, SUB, DIV, MUL }
 
-    fun add(firstOperand: Double, secondOperand: Double): Double = firstOperand + secondOperand
+    open fun add(firstOperand: Double, secondOperand: Double): Double = firstOperand + secondOperand
 
-    fun sub(firstOperand: Double, secondOperand: Double): Double = firstOperand - secondOperand
+    open fun sub(firstOperand: Double, secondOperand: Double): Double = firstOperand - secondOperand
 
-    fun div(firstOperand: Double, secondOperand: Double): Double = firstOperand / secondOperand
+    open fun div(firstOperand: Double, secondOperand: Double): Double = firstOperand / secondOperand
 
-    fun mul(firstOperand: Double, secondOperand: Double): Double = firstOperand * secondOperand
+    open fun mul(firstOperand: Double, secondOperand: Double): Double = firstOperand * secondOperand
 }
